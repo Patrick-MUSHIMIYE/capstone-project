aeb644960bea55474c9c2a4ee6ac331c35b06345
MUSHIMIYE Patrick <p.mushimiy@alustudent.com>
2024-01-16 16:10:31 +0200

    perf: models and onboard page created

---
+++ capst_project/lostfound_app/models.py
@@ -0,0 +1,17 @@
+# create models
+
+from django.db import models
+from django.utils import timezone
+
+class upload_image(models.Model):
+    first_name = models.CharField(max_length=254)
+    second_name = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    location = models.CharField(max_length=254)
+    tel = models.IntegerField()
+    upload_images = models.ImageField(blank=True, null=True, upload_to='uploads')
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.second_name}'
+    
