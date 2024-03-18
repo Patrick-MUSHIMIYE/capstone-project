d68ed888753704c1418760d1846b9a00ebc71017
MUSHIMIYE Patrick <p.mushimiy@alustudent.com>
2024-03-18 10:49:05 +0200

    perf: added configuration for vercel

--- capst_project/capst_project/settings.py
+++ capst_project/capst_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xyw@l@--chii_1pc8fi14&6919uom@k@f4o(4*@l^*sc+67b#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -132,6 +132,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
