e63f5bfe8716eb4d54d54eb568e7e850724193fe
MUSHIMIYE Patrick <p.mushimiy@alustudent.com>
2024-03-30 11:29:52 +0200

    refactor: Switch from mailtrap sending email to Gmail

--- capst_project/capst_project/settings.py
+++ capst_project/capst_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import pytesseract
+from credentials import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,12 +152,20 @@ LOGOUT_REDIRECT_URL = 'login'
 # EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
-# Django email settings for Mailtrap
-EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER = 'fa7b8c8c584b7c'
-EMAIL_HOST_PASSWORD = '74e306d47e2898'
-EMAIL_PORT = '2525'
-DEFAULT_FROM_EMAIL = "techteam@lostfound.com"
+# # Django email settings for Mailtrap
+# EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+# EMAIL_HOST_USER = 'fa7b8c8c584b7c'
+# EMAIL_HOST_PASSWORD = '74e306d47e2898'
+# EMAIL_PORT = '2525'
+# DEFAULT_FROM_EMAIL = "techteam@lostfound.com"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # For TLS
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = EMAIL_HOST_USER,
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+
 
 # Set Tesseract path
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR'
---
+++ capst_project/credentials.py
@@ -0,0 +1,2 @@
+EMAIL_HOST_USER = 'p.mushimiy@alustudent.com'  
+EMAIL_HOST_PASSWORD = 'psvb chon kmjz jrvp' 
